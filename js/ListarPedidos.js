62cc31c15311f991e030bcaa70378736e3b2f12f
LuizNola <luizn647@gmail.com>
2021-04-04 15:21:16 -0300

    Trocando a foto de cada pedido

--- js/ListarPedidos.js
+++ js/ListarPedidos.js
@@ -21,14 +21,33 @@ async function listarPedidos(){
     .then(result => result)
     .catch(error => console.log('error', error));
 
+    var imgsJson = {
+        "images": 
+        [
+            "http://i.mlcdn.com.br/portaldalu/fotosconteudo/68927.jpg",
+        "https://ciclovivo.com.br/wp-content/uploads/2017/01/16129802_1388747494503920_740414504_o.jpg",
+        "https://www.guarapari.es.gov.br/uploads/files/noticias/material-escolar.png",
+        "https://ccjmt.com.br/wp-content/uploads/2017/12/materiais-1200x675.jpg",
+        "https://www.cm-moncorvo.pt/cmtorremoncorvo/uploads/news/image/709/escolar_760x450.jpg",
+        "https://catracalivre.com.br/wp-content/thumbnails/DB2vFIkCMS3euIo5rz7aWpW7nlE=/wp-content/uploads/2017/01/iStock-505261864.jpg"
+    ]
+    }
+
+    console.log(imgsJson)
+
     
     element.map((item, i) =>{
+
+        let img = imgsJson.images[Math.floor(Math.random() *imgsJson.images.length)]
+
+
         let CardPedido = document.querySelector('.material-card').cloneNode(true)
         CardPedido.setAttribute('data-key', i);
 
         CardPedido.querySelector('.material-card .material-lista strong').innerHTML = item.material_list
         CardPedido.querySelector('.material-card .cidade strong').innerHTML = item.address
-
+        CardPedido.querySelector('.material-card .material-img').src = img
+        
 
         document.querySelector('.main-pedidos ul').append(CardPedido)
 
